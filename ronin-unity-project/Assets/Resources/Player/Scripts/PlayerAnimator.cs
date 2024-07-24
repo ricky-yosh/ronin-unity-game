8e531b2c4a7cf02b0cf06211d597118379b2cc1f
Ricky Yoshioka <63967513+ricky-yosh@users.noreply.github.com>
2024-07-23 20:55:05 -0700

    Create animations controller and update animator bool every frame

--- ronin-unity-project/Assets/Resources/Player/Scripts/PlayerAnimator.cs
+++ ronin-unity-project/Assets/Resources/Player/Scripts/PlayerAnimator.cs
@@ -18,6 +18,11 @@ namespace RoninGame
 
         private Animator animator;
 
+        private void Update()
+        {
+            animator.SetBool(PlayerAnimationStates.IS_RUNNING, playerMovement.getIsRunning());
+        }
+
         private void Awake()
         {
             animator = GetComponent<Animator>();
